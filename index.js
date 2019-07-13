ba258f92bb7f47d13fb2ad09dac7b669c1d0f7d0
Songsak Chueanueon <boobeezaza41@gmail.com>
2019-07-13 13:55:47 +0700

    Hello Github

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => res.send('Songsak Chueaueon'))
+app.get('/about', (req, res) => res.send('หน้า เกี่ยวกับ'))
+app.get('/contact', (req, res) => res.send('หน้า ติดต่อเรา'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
